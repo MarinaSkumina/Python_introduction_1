e84188cba264b72a9feab587a219f85da04e6dde
Maryna Skumina <maragnchr@gmail.com>
2025-05-30 12:13:25 +0200

    add path entering functions to creating tables module

--- Creating tables.py
+++ Creating tables.py
@@ -1,7 +1,6 @@
 import os.path
 import json
 import psycopg2
-import datetime
 
 # Connect to postgres DB
 conn = psycopg2.connect(dbname='students_rooms',
@@ -20,28 +19,34 @@ cur.execute('''
 	room_name text
 	);
 ''')
-print("Table created successfully")
+print("Table rooms created successfully")
 conn.commit()
 
+#path_rooms = "./rooms.json"
+
 # Reading json file rooms.json
-if os.path.exists("./rooms.json"):
-    with open("./rooms.json", encoding="UTF-8") as file_json:
-        rooms = json.load(file_json)
+def path_input_rooms () -> None:
+    print("Please, enter the path to the rooms data file")
+    path_rooms = input()
+    if os.path.exists(path_rooms):
+        with open(path_rooms, encoding="UTF-8") as file_json:
+            rooms = json.load(file_json)
+        # Inserting information from json to table rooms through SQL-query
+        for obj in rooms:
+            cur.execute(
+                "INSERT INTO rooms (room_id, room_name) VALUES (%s, %s)", (obj['id'], obj['name'])
+            )
+        conn.commit()
+        print(path_rooms)
+    else:
+        print("The path is incorrect or not exists")
+        path_input_rooms ()
 
-# Inserting information from json to table rooms through SQL-query
-for obj in rooms:
-    cur.execute(
-        "INSERT INTO rooms (room_id, room_name) VALUES (%s, %s)", (obj['id'], obj['name'])
-    )
-    conn.commit()
+path_input_rooms()
 
+
+# Checking if data is saved
 cur.execute("select * from rooms limit 1;")
-
-# Getting the query result
-version = cur.fetchone()
-print(version)
-
-cur.execute("select * from rooms where room_id = 999;")
 
 # Getting the query result
 version = cur.fetchone()
@@ -61,30 +66,36 @@ cur.execute('''
 	room_id integer references rooms (room_id)
 	);
 ''')
-print("Table created successfully")
+print("Table students created successfully")
 conn.commit()
 
 # Reading json file students.json
-if os.path.exists("./students.json"):
-    with open("./students.json", encoding="UTF-8") as file_json:
-        students = json.load(file_json)
+#students_path = "./students.json"
 
-# Inserting information from json to table students through SQL-query
-for obj in students:
-    cur.execute(
-        "INSERT INTO students (student_id, birthday, student_name, sex, room_id) VALUES (%s, %s, %s, %s, %s)", \
-        (obj['id'], obj['birthday'], obj['name'], obj['sex'], obj['room'])
-    )
-    conn.commit()
+def path_input_students () -> str:
+    print("Please, enter the path to the students data file")
+    students_path = input()
+    if os.path.exists(students_path):
+        with open(students_path, encoding="UTF-8") as file_json:
+            students = json.load(file_json)
+        # Inserting information from json to table students through SQL-query
+        for obj in students:
+            cur.execute(
+                "INSERT INTO students (student_id, birthday, student_name, sex, room_id) VALUES (%s, %s, %s, %s, %s)", \
+                (obj['id'], obj['birthday'], obj['name'], obj['sex'], obj['room'])
+            )
+            conn.commit()
+    else:
+        print("The path is incorrect or not exists")
+        path_input_students()
 
-#cur.execute('''
- #   UPDATE students
- #   SET birthday = DATE(birthday);
-#''')
 
+path_input_students()
+
+# Checking if data is saved
 cur.execute("select * from students limit 1;")
 
-# Получение результата запроса
+# Getting query result
 version = cur.fetchone()
 print(version)
 
