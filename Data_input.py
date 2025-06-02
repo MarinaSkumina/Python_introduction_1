85b0600039c3928acfbffaae002b92315d2177b7
Maryna Skumina <maragnchr@gmail.com>
2025-06-02 14:49:59 +0200

    edit data input

--- Data_input.py
+++ Data_input.py
@@ -2,42 +2,41 @@ import os.path
 import json
 from DB_connection import connection
 
-
-# Reading json file rooms.json
-    def path_input_rooms () -> None:
-        print("Please, enter the path to the rooms data file")
-        path_rooms = input()
-        #path_rooms = "data/rooms.json"
-        if os.path.exists(path_rooms):
-            with open(path_rooms, encoding="UTF-8") as file_json:
-                rooms = json.load(file_json)
-            # Inserting information from json to table rooms through SQL-query
-            for obj in rooms:
-                cur.execute(
-                    "INSERT INTO rooms (room_id, room_name) VALUES (%s, %s)", (obj['id'], obj['name'])
-                )
-            conn.commit()
-            print(path_rooms)
-        else:
-            print("The path is incorrect or not exists")
-            path_input_rooms ()
-
-
-    path_input_rooms()
-
+@connection
+def path_input_rooms(conn) -> None:
+    path_rooms = input("Please, enter the path to the rooms data file (push Enter for meaning by default:)")
+    if not path_rooms.strip():
+        path_rooms = 'data/rooms.json'
+    cur = conn.cursor()
+    if os.path.exists(path_rooms):
+        with open(path_rooms, encoding="UTF-8") as file_json:
+            rooms = json.load(file_json)
+        # Inserting information from json to table rooms through SQL-query
+        for obj in rooms:
+            cur.execute(
+                "INSERT INTO rooms (room_id, room_name) VALUES (%s, %s)", (obj['id'], obj['name'])
+            )
+        conn.commit()
+        print(path_rooms)
+    else:
+        print("The path is incorrect or not exists")
+        path_input_rooms ()
     # Checking if data is saved
     cur.execute("select * from rooms limit 1;")
-
     # Getting the query result
     version = cur.fetchone()
     print(version)
 
 
+path_input_rooms()
+
 # Reading json file students.json
-def path_input_students() -> str:
-    print("Please, enter the path to the students data file")
-    students_path = input()
-    # students_path = "data/students.json"
+@connection
+def path_input_students(conn) -> str:
+    students_path = input("Please, enter the path to the students data file (push Enter for meaning by default:)")
+    if not students_path.strip():
+        students_path = "data/students.json"
+    cur = conn.cursor()
     if os.path.exists(students_path):
         with open(students_path, encoding="UTF-8") as file_json:
             students = json.load(file_json)
@@ -51,14 +50,13 @@ def path_input_students() -> str:
     else:
         print("The path is incorrect or not exists")
         path_input_students()
+    # Checking if data is saved
+    cur.execute("select * from students limit 1;")
+    # Getting query result
+    version = cur.fetchone()
+    print(version)
 
 
 path_input_students()
 
-# Checking if data is saved
-cur.execute("select * from students limit 1;")
 
-# Getting query result
-version = cur.fetchone()
-print(version)
-cur.close()
