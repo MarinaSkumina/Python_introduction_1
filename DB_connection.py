2039fee322768676e4bc610acd783d887c5b36f5
Maryna Skumina <maragnchr@gmail.com>
2025-06-02 10:00:23 +0200

    add try except to db connection

--- DB_connection.py
+++ DB_connection.py
@@ -3,12 +3,16 @@ import psycopg2
 def connection(func):
     def wrapper(*args, **kwargs):
         # Connect to postgres DB
-        conn = psycopg2.connect(dbname='students_rooms',
-                                 host="localhost",
-                                 user='postgres',
-                                 password='password',
-                                 port=5432)
-        result = func(conn, *args, **kwargs)
-        conn.close()
-        return result
+        try:
+            conn = psycopg2.connect(dbname='students_rooms',
+                                     host="localhost",
+                                     user='postgres',
+                                     password='password',
+                                     port=5432)
+            result = func(conn, *args, **kwargs)
+            conn.close()
+            return result
+        except Exception as e:
+            # DB connection error
+            print(f"The error has occurred. Details: {e}")
     return wrapper
